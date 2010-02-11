608435ee52bfba2b8f25b9000420d403c977615e
Manfred Stienstra <manfred@fngtps.com>
2010-02-11 13:39:11 +0100

    Add Locke::Subversion::Repository#changes.
    Locke::Subversion::Repository#changes returns added an removed lines for each commit.

--- lib/locke/subversion.rb
+++ lib/locke/subversion.rb
@@ -10,12 +10,22 @@ module Locke
         @path = path
       end
       
+      def changes
+        if last_revision > 1
+          (2..last_revision).map do |revision|
+            self.class.count_changes(svn("diff -x -b --change #{revision} #{@path}"))
+          end
+        else
+          []
+        end
+      end
+      
       def last_revision
         attributes['Revision'].to_i
       end
       
       def attributes
-        self.class.parse_attributes(svn("info #{@path}"))
+        @attributes ||= self.class.parse_attributes(svn("info #{@path}"))
       end
       
       def self.parse_attributes(input)
@@ -23,6 +33,22 @@ module Locke
           key, value = line.split(':')
           attributes[key.strip] = value.strip
           attributes
+        end
+      end
+      
+      def self.count_changes(input)
+        input.split("\n").inject({:added => 0, :removed => 0}) do |changes, line|
+          if %w(--- +++).include?(line[0,3])
+            changes
+          else
+            case line[0,1]
+            when '+'
+              changes[:added] += 1
+            when '-'
+              changes[:removed] += 1
+            end
+            changes
+          end
         end
       end
     end
--- test/subversion_test.rb
+++ test/subversion_test.rb
@@ -13,7 +13,16 @@ describe "Locke::Subversion::Repository" do
     FileUtils.rm_rf(File.join(Test.root, 'fixtures', 'svn-project-checkout'))
   end
   
-  it "should get the last revision of a subversion repository" do
+  it "should return the last revision of a subversion repository" do
     @repository.last_revision.should == 5
+  end
+  
+  it "should return a list changes to the repository" do
+    @repository.changes.should == [
+      {:added=>1, :removed=>0},
+      {:added=>1, :removed=>1},
+      {:added=>5, :removed=>1},
+      {:added=>1, :removed=>3}
+    ]
   end
 end
